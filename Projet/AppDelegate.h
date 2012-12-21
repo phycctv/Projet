bbb84b7fe1c0885210eb00a3a84b6e74b324a61f
Xiaowei ZHANG <phycctv@xiaomatoiMac.local>
2012-12-21 14:37:59 +0100

    hoho

--- Projet/AppDelegate.h
+++ Projet/AppDelegate.h
@@ -3,7 +3,7 @@
 //  Projet
 //
 //  Created by Xiaowei ZHANG on 12-12-21.
-//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved .
 //
 
 #import <UIKit/UIKit.h>
